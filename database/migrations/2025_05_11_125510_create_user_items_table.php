6744325839d7e41090179400b2c2c6f0cb9bf905
ZPuh <muhammadzuhdi391@gmail.com>
2025-05-18 19:17:03 +0800

    Menambahkan kolom role di tabel useritem

--- database/migrations/2025_05_11_125510_create_user_items_table.php
+++ database/migrations/2025_05_11_125510_create_user_items_table.php
@@ -6,22 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('user_items', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->foreignId('item_id')->constrained('items')->onDelete('cascade');
+            $table->enum('role', ['pelapor', 'penemu', 'pemilik'])->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('user_items');
